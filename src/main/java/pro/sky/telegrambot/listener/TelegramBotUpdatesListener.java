4f0287d1fa09001f6b9d570ebfcdbd878e27e915
MaraOD555 <ruffbc@hotmail.com>
2022-12-06 08:29:53 +0300

    telegram bot created

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -6,7 +6,7 @@ import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.model.NotificationTask;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
@@ -14,6 +14,8 @@ import pro.sky.telegrambot.repository.NotificationTaskRepository;
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,13 +25,14 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
 
     private final NotificationTaskRepository notificationTaskRepository;
-
-    public TelegramBotUpdatesListener(NotificationTaskRepository notificationTaskRepository) {
+    private final Pattern pattern = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W+]+)"); // regular expression
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"); // dataFormat
+    public TelegramBotUpdatesListener(NotificationTaskRepository notificationTaskRepository, TelegramBot telegramBot) {
         this.notificationTaskRepository = notificationTaskRepository;
+        this.telegramBot = telegramBot;
     }
 
     @PostConstruct
@@ -44,30 +47,35 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             Long chatId = update.message().chat().id(); // receiving the chat and its id
             String inputText = update.message().text(); // receiving an input message
             Long id = 0L;
-
+            Matcher matcher = pattern.matcher(inputText);//checking the input text
             if (inputText.startsWith("/start")) {
                 SendMessage message = new SendMessage(chatId, "Hi! Our conversation is active");
                 // toward which chat and which exact message to send (as a response of the input message)
                telegramBot.execute(message); // sending the message
-                /* try {
-                    telegramBot.execute(message);   //Actually sending the message
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();      //Any error will be printed here
-                }*/
-            } else {
-                Pattern pattern = Pattern.compile("([0-9.:\\s]{16})(\\s)([\\W+]+)");// regular expression, searching
-                Matcher matcher = pattern.matcher(inputText);//checking the input text
-                if (matcher.matches()){ // if matches, division the result into two groups
+            } else if (matcher.matches()){ // if matches, division the result into two groups
                 String dateTime = matcher.group(1);// date
                 String textReminder = matcher.group(3); // text
-                LocalDateTime localDateTime = LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
-                NotificationTask notificationTask = new NotificationTask(id, chatId, localDateTime, textReminder);
-                notificationTaskRepository.save(notificationTask);
-                }
+                LocalDateTime localDateTime = LocalDateTime.parse(dateTime, dateTimeFormatter); // creating localDateTime
+                NotificationTask notificationTask = new NotificationTask(id, chatId, localDateTime, textReminder); // creating new item
+                notificationTaskRepository.save(notificationTask); // saving a new item in DB via repository
+                } else { // code in case incorrect input message
+                SendMessage message = new SendMessage(chatId, "Data format is incorrect! Please, check.");
+                telegramBot.execute(message);
             }
         });
                 return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+    @Scheduled(cron = "0 0/1 * * * *") // set up a schedule (cron for every minute)
+    public void notificationsToBeSend() {
+        LocalDateTime dateTime = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES); // limit for time - minutes
+        Collection<NotificationTask> toBeSent = notificationTaskRepository.findAllByDateTime(dateTime); // collection of appropriate messages by dateTime
+        toBeSent
+                .forEach(notificationTask -> { // get chatId and textReminder for each member of list and send them according to the data
+                    telegramBot.execute(new SendMessage(notificationTask.getChatId(), notificationTask.getTextReminder()));
+                    logger.info("SendOut activated: <{}>, to chat: {}.", notificationTask.getTextReminder(), notificationTask.getChatId());
+                });
+    }
+
 }
 
 
--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -4,8 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.model.NotificationTask;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
 
 @Repository
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+
+    Collection<NotificationTask> findAllByDateTime (LocalDateTime dateTime);
 
 }
